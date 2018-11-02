c45afa5e5a3f38c34512d8b1321b8d271b2e9a0f
Magz8984 <collinsmuthinja@gmail.com>
2018-11-02 19:42:22 +0300

    Add App Screens

---
+++ app/src/main/java/com/example/collins/agrino/RecyclerViewAdapter.java
@@ -0,0 +1,29 @@
+package com.example.collins.agrino;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/collins/agrino/SpashScreen.java
@@ -0,0 +1,32 @@
+package com.example.collins.agrino;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SpashScreen extends AppCompatActivity {
+
+    @BindView(R.id.pgStart) ProgressBar pgStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+        ButterKnife.bind(this);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SpashScreen.this,MainActivity.class);
+                startActivity(i);
+            }
+        },3000);
+    }
+}
