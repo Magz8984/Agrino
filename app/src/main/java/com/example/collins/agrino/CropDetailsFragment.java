4b3afb0f1a7ff175ebd46e046f6afc7de1b1dc6f
Magz8984 <collinsmuthinja@gmail.com>
2018-11-09 17:52:01 +0300

    Add Fragment

---
+++ app/src/main/java/com/example/collins/agrino/CropDetailsFragment.java
@@ -0,0 +1,26 @@
+package com.example.collins.agrino;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CropDetailsFragment extends Fragment {
+
+
+    public CropDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_crop_details, container, false);
+    }
+
+}
--- app/src/main/java/com/example/collins/agrino/MainActivity.java
+++ app/src/main/java/com/example/collins/agrino/MainActivity.java
@@ -43,8 +43,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-
     @Override
     public void onBackPressed(){
         moveTaskToBack(false);
